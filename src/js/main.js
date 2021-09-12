342d1a66775911708ad8471d84ecbb6b01280e5a
taniamg <taniamg81@gmail.com>
2021-09-12 12:34:02 +0200

    upload favourite and LS fixed

--- src/js/main.js
+++ src/js/main.js
@@ -16,23 +16,23 @@ function paintHtml() {
   html += `<section>`;
   html += `<div class="search_container" >`;
   html += `<h1> Number 5 is alive. What do I search for you?</h1>`;
-  html += `<form class="form_container js_form">`;
+  html += `<form autocomplete="off" class="form_container js_form">`;
   html += `<label for="search request"> </label>`;
-  html += `<input class="text js_text" type="text" placeholder="Fringe" />`;
+  html += `<input id="search request" class="form_container--text js_text" type="text" placeholder="Fringe" />`;
   html += `<label for="submit"> </label>`;
-  html += `<input class="button js_btn" type="submit" value="Buscar" />`;
+  html += `<input id ="submit" class="form_container--button js_btn" type="submit"  />`;
   html += `</form >`;
   html += `</div>`;
   html += `</section>`;
   html += `<section class="main_series--container">`;
   html += `<div class="favourites_container">`;
   html += `<h2> Series favoritas: </h2>`;
-  html += `<ul class="fav_list--series fav_series--container">`;
+  html += `<ul class="favourites_container--series fav_list--series">`;
   html += `</ul>`;
   html += `</div>`;
   html += `<div class="series_container">`;
   html += `<h2> Hemos encontrado.... </h2>`;
-  html += `<ul class="list_series list_series--container">`;
+  html += `<ul class="series_container--list list_series">`;
   html += `</ul>`;
   html += `</div>`;
   html += `</section>`;
@@ -49,20 +49,26 @@ const inputEl = document.querySelector(".js_text");
 const series = document.querySelector(".list_series");
 const favElements = document.querySelector(".fav_list--series");
 // 6. declaro la funcion que llama a la API y almacena result en dataSeries
+
+btnSearch.addEventListener("click", handleButtonSearch);
+form.addEventListener("submit", preventD);
+
+function handleButtonSearch(ev) {
+  console.log("cacaSearch");
+  getFromApi();
+}
+
+function preventD(event) {
+  console.log("cacaPrevent");
+  event.preventDefault();
+}
+
 function getFromApi() {
   const url = "https://api.tvmaze.com/search/shows?q=" + inputEl.value;
 
   //eventos
 
-  function handleButtonSearch(ev) {
-    getFromApi();
-  }
-  btnSearch.addEventListener("click", handleButtonSearch);
-
-  function preventD(event) {
-    event.preventDefault();
-  }
-  form.addEventListener("submit", preventD);
+  console.log("caca1");
 
   fetch(url)
     .then((response) => response.json())
@@ -76,10 +82,10 @@ function getFromApi() {
 //funcion para pintar series en ul
 function paintSeries() {
   let seriesList = "";
-
+  //console.log(dataSeries);
   for (const iten of dataSeries) {
-    console.log(iten);
-    seriesList += `<li class="series_place favourites " id=${iten.show.id}>`;
+    //console.log(iten);
+    seriesList += `<li class="series_place favourites js_favourites" id=${iten.show.id}>`;
 
     if (iten.show.image === null) {
       seriesList += `<img src ="${imageDefault}">`;
@@ -98,16 +104,17 @@ function paintSeries() {
 
 // funcion manejadora del evento que va a escuchar cada serie, elegirla y añadirla a fav
 function handleFavSeries(ev) {
+  //console.log("Pulso boton");
   //variable para obtener el id de cada serie clicada
-  const selectedSerie = parseInt(ev.target.parentElement.id);
+  const selectedSerie = parseInt(ev.currentTarget.id);
   //busco la serie en el array de series
   const clickedSerie = dataSeries.find((iten) => {
-    iten.show.id === selectedSerie;
+    return iten.show.id === parseInt(selectedSerie);
   });
 
   // busco la serieclicada en el array de favoritos
   const favSeriesFound = favouritesSeries.findIndex((favIten) => {
-    return favIten.show.id === selectedSerie;
+    return favIten.show.id === parseInt(selectedSerie);
   });
   //si no esta me devuelve -1
   if (favSeriesFound === -1) {
@@ -118,10 +125,9 @@ function handleFavSeries(ev) {
     favouritesSeries.splice(favSeriesFound, 1);
   }
   paintFavSeries();
-  paintSeries();
   setInLocalStorage();
 }
-debugger;
+
 //funcion que reconozca evento click sobre los li de cada  serie
 function listenSerieList() {
   const favSeriesElements = document.querySelectorAll(".js_favourites");
@@ -160,12 +166,11 @@ function paintFavSeries() {
     } else {
       favElement += `<img src ="${eachSerie.show.image.medium}">`;
     }
-    favElements += `<h2>${eachSerie.show.name}</h2>`;
-    favElements += `</li>`;
+    favElement += `<h2>${eachSerie.show.name}</h2>`;
+    favElement += `</li>`;
   }
   favElements.innerHTML = favElement;
 }
-paintFavSeries();
 
 // localStorage
 
@@ -182,7 +187,7 @@ function getLocalStorage() {
   const localStorageSeries = localStorage.getItem("favouritesSeries");
   if (localStorageSeries === null) {
     //no hay datos asi que ejecutamos peticion a API
-    getFromApi();
+    //getFromApi();
   } else {
     //si hay datos los guardo en una variable y loincluyo en el array favoritos
     const arrayPreviousFav = JSON.parse(localStorageSeries);
